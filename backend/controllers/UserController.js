5056fb7faca076d8e0993f0a8dfbec335b46c5ea
LuccaMenezes <luccamen2016@gmail.com>
2023-03-04 01:35:19 -0300

    adicionado rota de login e validações

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -69,4 +69,37 @@ module.exports = class UserController {
       res.status(500).json({ message: error })
     }
   }
+
+  static async login(req, res) {
+   const email = req.body.email
+   const password = req.body.password
+
+   if (!email) {
+     res.status(422).json({ message: 'O e-mail é obrigatório!' })
+     return
+   }
+
+   if (!password) {
+     res.status(422).json({ message: 'A senha é obrigatória!' })
+     return
+   }
+
+   // check if user exists
+   const user = await User.findOne({ email: email })
+
+   if (!user) {
+     return res
+       .status(422)
+       .json({ message: 'Não há usuário cadastrado com este e-mail!' })
+   }
+
+   // check if password match
+   const checkPassword = await bcrypt.compare(password, user.password)
+
+   if (!checkPassword) {
+     return res.status(422).json({ message: 'Senha inválida' })
+   }
+
+   await createUserToken(user, req, res)
+ }
 }
