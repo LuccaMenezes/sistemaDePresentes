8d96f730fcba80ce3fb5acd17578b515d8bac0eb
LuccaMenezes <luccamen2016@gmail.com>
2023-03-04 01:00:39 -0300

    Controller e rotas dos Users

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 
+const UserRoutes = require('./routes/UserRoutes')
+
 const app = express()
 
 // Config JSON response
@@ -13,5 +15,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static('public'))
 
 // Routes
+
+app.use('/users', UserRoutes)
 
 app.listen(5000)
