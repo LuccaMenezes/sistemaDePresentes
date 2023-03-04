f3b6436327174436cb4e59d181f4fe6a4f09a4c5
LuccaMenezes <luccamen2016@gmail.com>
2023-03-04 07:25:34 -0300

    complete getUserById

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -125,4 +125,17 @@ module.exports = class UserController {
    res.status(200).send(currentUser)
  }
 
+ static async getUserById(req, res) {
+  const id = req.params.id
+
+  const user = await User.findById(id).select('-password')
+
+  if (!user) {
+    res.status(422).json({ message: 'Usuário não encontrado!' })
+    return
+  }
+
+  res.status(200).json({ user })
+}
+
 }
